d683baeba9a4350f313af7a4d71b663e519fa00c
вапвап <gurban0v@yandex.ru>
2016-06-07 13:15:39 +0300

    функция2

--- build/js/check.js
+++ build/js/check.js
@@ -1,16 +1,30 @@
 function getMessage(a,b) {
-	if(a == 'boolean') {
-		return 'Переданное GIF-изображение анимировано и содержит' + b + 'кадров'; 
-	}else{
+	if(typeof a === 'boolean') {
+		if(a){
+			return 'Переданное GIF-изображение анимировано и содержит' + b + 'кадров'; 
+		}else{
 		return 'Переданное GIF-изображение не анимировано';
 	}
-	if(a == 'number') {
+	if(typeof a === 'number') {
 		return 'Переданное SVG-изображение содержит' + a + 'объектов и ' + b * 4 + 'атрибутов';
 	}
-	if(a == 'array') {
-		return 'Количество красных точек во всех строчках изображения: ' [sum];
+
+
+	if(a instanceof Array && !(b instanceof Array) {
+		var sum = 0;
+		aLength = a.length;
+		for (var i = 0; i < aLength - 1; i++) {
+			sum += a[i];
+		}
+		return 'Количество красных точек во всех строчках изображения: ' + sum;
 	}
-	if(a == 'array' && b == 'array'){
-		return 'Общая площадь артефактов сжатия: ' [square] 'пикселей';
+
+	if(a instanceof Array && b instanceof Array){
+		var square = 0;
+		for(var i = 0; i < a.length; i++){
+			square += (a[i]*b[i]);
+		}
+
+		return 'Общая площадь артефактов сжатия: ' + square + 'пикселей';
 	}
 };
